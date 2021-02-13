4fef401006ac5e5774663768df4d0dce1a1862ae
sam <sam@freighttrust.com>
2021-02-13 01:44:55 -0800

    feat(init): commit

---
+++ .stylelint.config.js
@@ -0,0 +1,28 @@
+
+{
+  "extends": "stylelint-config-standard",
+  "rules": {
+    "indentation": 4,
+    "string-quotes": "double",
+    "no-duplicate-selectors": true,
+    "color-hex-case": "lower",
+    "color-hex-length": "long",
+    "selector-no-combinator": true,
+    "selector-combinator-space-after": "always",
+    "selector-attribute-quotes": "always",
+    "selector-attribute-operator-space-before": "never",
+    "selector-attribute-operator-space-after": "never",
+    "selector-attribute-brackets-space-inside": "never",
+    "declaration-colon-space-before": "never",
+    "declaration-colon-space-after": "always",
+    "number-leading-zero": "always",
+    "function-url-quotes": "always",
+    "comment-whitespace-inside": "always",
+    "comment-empty-line-before": "always",
+    "selector-pseudo-class-parentheses-space-inside": "always",
+    "selector-no-universal": true,
+    "media-feature-parentheses-space-inside": "never",
+    "media-feature-colon-space-before": "never",
+    "media-feature-colon-space-after": "never"
+  }
+}
