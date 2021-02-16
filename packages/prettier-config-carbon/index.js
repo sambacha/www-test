88a78e73e5ec71196fc0a62c18bca3d93e768d75
sam bacha <sam@freighttrust.com>
2021-02-16 07:41:42 -0800

    fix(cfg): prettier

--- packages/prettier-config-carbon/index.js
+++ packages/prettier-config-carbon/index.js
@@ -1,16 +1,17 @@
 /**
- * Copyright IBM Corp. 2016, 2020
- *
- * This source code is licensed under the Apache-2.0 license found in the
- * LICENSE file in the root directory of this source tree.
+ * 
  */
 
 'use strict';
 
 module.exports = {
   jsxBracketSameLine: true,
-  printWidth: 80,
+  printWidth: 100,
   singleQuote: true,
   trailingComma: 'es5',
   proseWrap: 'always',
+  tabWidth: '2',
+  useTabs: false,
+  bracketSpacing: true,
+  endOfLine: 'lf'
 };
