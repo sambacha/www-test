7f7fec5ceeca990768afd020f722868b47c30ed7
sam bacha <sam@freighttrust.com>
2021-04-05 02:52:08 -0700

    fix(config): no IE

--- packages/browserslist-config/index.js
+++ packages/browserslist-config/index.js
@@ -1,3 +1,21 @@
 'use strict';
 
-module.exports = ['last 1 versions', 'ie >= 11', 'Firefox ESR'];
+module.exports = ['>0.3%', 'not dead', 'not op_mini all', 'not IE > 0'];
+/** @exports browserlist-config */
+/**
+npx browserslist ">0.3%, not dead, not op_mini all, not IE > 0"
+and_chr 89
+and_uc 12.12
+chrome 88
+chrome 87
+chrome 86
+edge 88
+firefox 85
+ios_saf 14.0-14.5
+ios_saf 13.4-13.7
+ios_saf 12.2-12.4
+opera 73
+safari 14
+safari 13.1
+samsung 13.0
+*/
