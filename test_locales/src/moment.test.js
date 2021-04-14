f415f7066c5a9a49d608db658cbfc35f54f1c8ce
sam bacha <sam@freighttrust.com>
2021-04-13 18:18:50 -0700

    Create moment.test.js

---
+++ test_locales/src/moment.test.js
@@ -0,0 +1,27 @@
+// src: https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
+const assert = require('assert');
+const moment = require('moment');
+require('moment/locale/ja');
+require('moment/locale/it');
+
+const oneDay = '2016-11-25T15:43:21';
+
+describe('localized moment', function() {
+  it('should be shown in English', function() {
+    moment.locale('en');
+    console.log(moment(oneDay).format('LLLL'));
+    assert(moment(oneDay).format('LLLL') === 'Friday, November 25, 2016 3:43 PM');
+  });
+
+  it('should be shown in Japanese', function() {
+    moment.locale('ja');
+    console.log(moment(oneDay).format('LLLL'));
+    assert(moment(oneDay).format('LLLL') === '2016年11月25日 15:43 金曜日');
+  });
+
+  it('should be shown in Italian', function() {
+    moment.locale('it');
+    console.log(moment(oneDay).format('LLLL'));
+    assert(moment(oneDay).format('LLLL') === 'venerdì 25 novembre 2016 15:43');
+  });
+});
